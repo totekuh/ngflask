c279dc5d395550ba210474077a0340d89ac5ca54
totekuh <downtownxore@gmail.com>
2021-12-22 19:58:27 +0100

    packaging the app to PyPI

---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name='ngflask',  # Required
+    version='1.0.1',  # Required
+    description='File-sharing tool',  # Optional
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    url='https://github.com/cyberhexe/ngflask',  # Optional
+    author='totekuh',  # Optional
+    author_email='totekuh@protonmail.com',  # Optional
+    classifiers=[  # Optional
+        'Development Status :: 5 - Production/Stable',
+
+        'Environment :: Console',
+
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Other Audience',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='file-sharing, ngrok, flask',  # Optional
+    package_dir={'': './src'},  # Optional
+    packages=find_packages(where='src'),  # Required
+    python_requires='>=3.6, <4',
+    install_requires=[
+        "flask",
+        'flask-autoindex',
+        "pyngrok"
+    ],  # Optional
+    entry_points={  # Optional
+        'console_scripts': [
+            'ngflask=ngflask.flask_ngrok:main',
+        ],
+    },
+
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/cyberhexe/revshell-generator/issues',
+        'Source': 'https://github.com/cyberhexe/revshell-generator',
+    },
+)
--- src/ngflask/flask_ngrok.py
+++ src/ngflask/flask_ngrok.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import glob
 import os
-from pyngrok import ngrok
+
 from flask import Flask
 from flask_autoindex import AutoIndex
+from pyngrok import ngrok
 
 DEFAULT_IP = "127.0.0.1"
 DEFAULT_PORT = 4200
@@ -13,36 +13,48 @@ DEFAULT_DIRECTORY = os.getcwd()
 def get_arguments():
     from argparse import ArgumentParser
     parser = ArgumentParser(description='Flask/Ngrok HTTP Server')
-    parser.add_argument("--ip",
+    parser.add_argument('-i',
+                        "--ip",
                         dest="ip",
                         required=False,
                         default=DEFAULT_IP,
                         type=str,
                         help="The local IP address to bind to. "
                              f"Default is {DEFAULT_IP}.")
-    parser.add_argument("--port",
+    parser.add_argument('-p',
+                        "--port",
                         dest="port",
                         required=False,
                         default=DEFAULT_PORT,
                         type=str,
                         help="The local TCP port to bind to. "
                              f"Default is {DEFAULT_PORT}.")
-    parser.add_argument("--directory",
+    parser.add_argument('-d',
+                        "--directory",
                         dest="directory",
                         required=False,
                         default=DEFAULT_DIRECTORY,
                         type=str,
                         help="The local directory to shave over the ngrok network. "
                              "Default is the current working directory.")
+    parser.add_argument('-pf',
+                        '--print-files',
+                        dest='print_files',
+                        action='store_true',
+                        required=False,
+                        help="Specify if the script should print URLs to files found in the shared directory "
+                             "for them to be copy-pasted. "
+                             "By default the script doesn't print them.")
     options = parser.parse_args()
     return options
 
 
 class TunneledHttpServer:
-    def __init__(self, ip, port, directory):
+    def __init__(self, ip: str, port: int, directory: str, print_files: bool = False):
         self.ip = ip
         self.port = port
         self.directory = directory
+        self.print_files = print_files
 
         self.app = Flask(__name__)
         AutoIndex(self.app, browse_root=self.directory)
@@ -56,18 +68,25 @@ class TunneledHttpServer:
         print(f" * Ngrok tunnel {self.public_url} -> http://{self.ip}:{self.port}/")
         print(f" * Serving files from the '{self.directory}' directory")
 
-        for current_path, folders, files in os.walk(self.directory):
-            for file in files:
-                relpath = os.path.relpath(os.path.join(current_path, file))
-                print(f" * Serving '{self.public_url}/{relpath}'")
+        if self.print_files:
+            for current_path, folders, files in os.walk(self.directory):
+                for file in files:
+                    relpath = os.path.relpath(os.path.join(current_path, file))
+                    relpath = relpath.replace(f'..{os.linesep}', '').replace(f"{self.directory[1:]}", '')
+                    print(f" * Serving '{self.public_url}/{relpath}'")
+        else:
+            print(f" * Hint: use -pf or --print-files for printing URLs for all the files shared over ngflask")
         self.app.config["BASE_URL"] = self.public_url
         self.app.run(host=self.ip, port=self.port)
 
 
-if __name__ == '__main__':
+def main():
     options = get_arguments()
-
     tunneled_http_server = TunneledHttpServer(ip=options.ip,
                                               port=options.port,
-                                              directory=options.directory)
+                                              directory=options.directory,
+                                              print_files=options.print_files)
     tunneled_http_server.start()
+
+if __name__ == '__main__':
+    main()
